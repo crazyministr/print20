d659851eaafc0ad01ccd4122ebf9af2c0685350b
Anton Malashenkov <ministr@Acer.(none)>
2013-04-18 19:50:11 +0400

    add checkbox

--- update_cover.php
+++ update_cover.php
@@ -2,7 +2,7 @@
 	require_once 'save_product.php';
 	$ch = $_GET['ch'];
 	if ($prod[$ch]->type != 'singlepage')
-		echo "<input type='checkbox' name='cover' id='cover' value='1'>";
+		echo "<input type='hidden' name='cover' id='cover' value='1'>";
 	else
-		echo "<input type='checkbox' name='cover' id='cover' value='0'>";
+		echo "<input type='hidden' name='cover' id='cover' value='0'>";
 ?>
