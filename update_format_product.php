1ac48d56a5797d34b9e48728d6151c93837029ad
khusain <khusain@hp.(none)>
2013-05-09 00:33:16 +0400

    ...

---
+++ update_format_product.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'save_product.php';
+$ch = $_GET['ch'];
+$cnt = count($prod[$ch]->formats);
+for ($i = 0; $i < $cnt; $i++) {
+    $w = $prod[$ch]->formats[$i];
+    $t = explode("x", $w);
+    $w = $t[1] . "x" . $t[0];
+    echo "<option value=" . $w . ">" . $w . "</option>";
+}
+?>
